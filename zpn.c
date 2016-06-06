96bb22886caf8e3fefec95ae7ad51aeb21e742ef
subhashinisss <subhashini.sss21@gmail.com>
2016-06-06 15:04:55 +0530

    Create zpn.c

---
+++ zpn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int t;
+  if(t==0)
+  {
+  printf("number is zero");
+  }
+  else if(t>0)
+  {
+  printf("number is positive");
+  }
+  else 
+  {
+  printf("number is negative");
+  }
+}
